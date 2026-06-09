f1e5781247b08625d866b8a1c9aceb9ac226bc3f
Developer <dev@hunter.rv>
2026-06-09 17:41:45 +0000

    chore: remove node_modules from git tracking

--- node_modules/@it-enterprise/digital-signature/euscp/EndUserCertificate.d.ts
+++
@@ -1,8 +0,0 @@
-import { EndUserCertificateInfoEx } from "./EndUserCertificateInfoEx";
-/**
- * Certificate.
- */
-export declare class EndUserCertificate {
-    infoEx: EndUserCertificateInfoEx;
-    data: Uint8Array;
-}
